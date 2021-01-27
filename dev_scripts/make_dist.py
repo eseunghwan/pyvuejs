175f9fb85610925cfb1b8f542cb2ccdd4fa39eee
eseunghwan <reignstarbits@gmail.com>
2021-01-27 18:23:47 +0900

    renewal to 2.0.0

--- dev_scripts/make_dist.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys, shutil
-
-__dir__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.chdir(__dir__)
-os.system(f"{sys.executable} ./setup.py clean --all")
-shutil.rmtree(os.path.join(__dir__, "dist"), ignore_errors = True)
-os.system(f"{sys.executable} ./setup.py bdist_wheel")
-os.system(f"{sys.executable} ./setup.py sdist")
